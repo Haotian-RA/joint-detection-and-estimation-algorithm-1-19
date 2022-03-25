fa9e28be3d1b728783a99945efdd6a62a6433cee
Haotian-RA <hzhai@masonlive.gmu.edu>
2022-03-25 14:11:18 +0800

    modify

--- main_matlab/bound_SD.m
+++ main_matlab/bound_SD.m
@@ -2,6 +2,8 @@ function [Bound_SD] = bound_SD(N,SNR,M,v,delta)
 % calculate bound for SD that is derived from paper.
 
 derichlet=sin(pi*delta*v)/sin(pi*delta);
+% derichlet=v;
+
 k=floor(2/3*N);
 mag_SNR=db2mag(2*SNR);
 Bound_SD=(v*M/4+derichlet^2*mag_SNR)*v*M./(4*pi^2*k^2*(N-k)/v*(mag_SNR).^2*derichlet^4);
